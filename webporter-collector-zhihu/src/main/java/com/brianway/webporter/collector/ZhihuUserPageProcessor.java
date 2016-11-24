ec78c056ed85f65ba99b4fd9cdd19ec323c75386
brianway <250902678@qq.com>
2016-11-24 23:25:24 +0800

    [add] add module: webporter-collector-zhihu to crawl zhihu user

---
+++ webporter-collector-zhihu/src/main/java/com/brianway/webporter/collector/ZhihuUserPageProcessor.java
@@ -0,0 +1,78 @@
+package com.brianway.webporter.collector;
+
+import com.brianway.webporter.configure.Configuration;
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
+import us.codecraft.webmagic.pipeline.FilePipeline;
+import us.codecraft.webmagic.processor.PageProcessor;
+import us.codecraft.webmagic.scheduler.QueueScheduler;
+import us.codecraft.webmagic.scheduler.component.BloomFilterDuplicateRemover;
+import us.codecraft.webmagic.selector.Json;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by brian on 16/11/24.
+ *
+ * 爬取知乎用户
+ */
+public class ZhihuUserPageProcessor implements PageProcessor {
+
+    private static String URL_TEMPLATE = "https://www.zhihu.com/api/v4/members/%s/followees?";
+
+    private static String QUERY_PARAMS = "per_page=20&include=data%5B%2A%5D.employments%2Ccover_url%2Callow_message%2Canswer_count%2Carticles_count%2Cfavorite_count%2Cfollower_count%2Cgender%2Cis_followed%2Cmessage_thread_token%2Cis_following%2Cbadge%5B%3F%28type%3Dbest_answerer%29%5D.topics&limit=0&offset=20";
+
+    private Site site = getConfigured();
+
+    public void process(Page page) {
+        Json json = page.getJson();
+        System.out.println(json);
+
+        page.putField("seg", json);
+
+        String isEnd = json.jsonPath("$.paging.is_end").get();
+        if (!Boolean.parseBoolean(isEnd)) {
+            page.addTargetRequest(json.jsonPath("$.paging.next").get());
+        }
+
+        List<String> urlTokens = json.jsonPath("$.data[*].url_token").all();
+        List<String> urls = generateMemberUrls(urlTokens);
+        page.addTargetRequests(urls);
+    }
+
+    public Site getSite() {
+        return site;
+    }
+
+    private Site getConfigured() {
+        Site site = Configuration.getConfiguredSite();
+        site.setRetryTimes(3).setSleepTime(10);
+        return site;
+    }
+
+    private static String generateMemberUrl(String urlToken) {
+        return String.format(URL_TEMPLATE, urlToken) + QUERY_PARAMS;
+    }
+
+    private static List<String> generateMemberUrls(List<String> urlTokens) {
+        List<String> urls = new ArrayList<String>(20);
+        for (String urlToken : urlTokens) {
+            urls.add(generateMemberUrl(urlToken));
+        }
+        return urls;
+    }
+
+    public static void main(String[] args) {
+        String pipelinePath = "/Users/brian/todo/data/webmagic";
+        int crawlSize = 1000000;
+        Spider.create(new ZhihuUserPageProcessor())
+                .setScheduler(new QueueScheduler()
+                        .setDuplicateRemover(new BloomFilterDuplicateRemover(crawlSize)))
+                .addPipeline(new FilePipeline(pipelinePath))
+                .addUrl(generateMemberUrl("hydro-ding"))
+                .thread(20)
+                .run();
+    }
+}
---
+++ webporter-collector-zhihu/src/test/java/com/brianway/webporter/collector/ZhihuUserPageProcessorTest.java
@@ -0,0 +1,49 @@
+package com.brianway.webporter.collector;
+
+import com.brianway.webporter.util.FileHelper;
+import org.junit.Assert;
+import org.junit.Test;
+import us.codecraft.webmagic.selector.Json;
+import us.codecraft.webmagic.selector.JsonPathSelector;
+
+import java.util.List;
+
+/**
+ * Created by brian on 16/11/24.
+ *
+ * 参考文档 http://webmagic.io/docs/zh/posts/chx-cases/js-render-page.html
+ * 两种写法都可以,后一种更简洁
+ */
+public class ZhihuUserPageProcessorTest {
+
+    @Test
+    public void testExtractJson() {
+        String dataFile = this.getClass().getResource("/").getPath() + "/sample-data.json";
+        String jsonText = FileHelper.getRawText(dataFile);
+        Json json = new Json(jsonText);
+
+        String isEnd = new JsonPathSelector("$.paging.is_end").select(jsonText);
+        Assert.assertEquals(false, Boolean.parseBoolean(isEnd));
+
+        //String nextUrl = new JsonPathSelector("$.paging.next").select(json);
+        String nextUrl = json.jsonPath("$.paging.next").get();
+        String expectedUrl = "https://www.zhihu.com/api/v4/members/hydro-ding/followees?per_page=10&include=data%5B%2A%5D.employments%2Ccover_url%2Callow_message%2Canswer_count%2Carticles_count%2Cfavorite_count%2Cfollower_count%2Cgender%2Cis_followed%2Cmessage_thread_token%2Cis_following%2Cbadge%5B%3F%28type%3Dbest_answerer%29%5D.topics&limit=10&offset=10";
+        Assert.assertEquals(expectedUrl, nextUrl);
+
+        //List<String> urlTokens = new JsonPathSelector("$.data[*].url_token").selectList(jsonText);
+        List<String> urlTokens = json.jsonPath("$.data[*].url_token").all();
+        String tokens = "[zingfood, yu-bing-43, Muyunio, dongx5, zhong-guo-ke-pu-bo-lan, kate0115, fan-fan-11-9, dr-song-41, pppp-76-22, boldyoung]";
+        Assert.assertEquals(tokens, urlTokens.toString());
+
+        //List<String> people = new JsonPathSelector("$.data[*].[*]").selectList(jsonText);
+        List<String> people = json.jsonPath("$.data[*].[*]").all();
+        System.out.println(people);
+        for (String s : people) {
+            System.out.println(s);
+        }
+
+    }
+
+
+
+}
