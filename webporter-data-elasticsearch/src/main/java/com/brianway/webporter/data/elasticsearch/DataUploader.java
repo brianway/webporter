951c10c5251fae0ac65f33b70dda9b0cc79ed21c
brianway <250902678@qq.com>
2016-11-29 23:37:57 +0800

    [add] add module:webporter-data-elasticsearch

---
+++ webporter-data-elasticsearch/src/main/java/com/brianway/webporter/data/elasticsearch/DataUploader.java
@@ -0,0 +1,28 @@
+package com.brianway.webporter.data.elasticsearch;
+
+import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.transport.InetSocketTransportAddress;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Created by brian on 16/11/29.
+ */
+public class DataUploader {
+    public static void main(String[] args) {
+        TransportClient client = null;
+        try {
+            // on startup
+            client = new PreBuiltTransportClient(Settings.EMPTY)
+                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("host1"), 9300))
+                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("host2"), 9300));
+            // on shutdown
+            client.close();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+    }
+}
