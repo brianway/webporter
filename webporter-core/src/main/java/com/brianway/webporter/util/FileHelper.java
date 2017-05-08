4d8ebca34f1fa97be2412e20969e4e61562d91e5
brianway <250902678@qq.com>
2017-05-08 19:48:20 +0800

    [add] add new methods in FileHelper for easy usage.

--- webporter-core/src/main/java/com/brianway/webporter/util/FileHelper.java
+++ webporter-core/src/main/java/com/brianway/webporter/util/FileHelper.java
@@ -1,13 +1,21 @@
 package com.brianway.webporter.util;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by brian on 16/11/24.
  */
 public class FileHelper {
+    private static final Logger logger = LoggerFactory.getLogger(FileHelper.class);
+
     public static String getRawText(String path) {
         BufferedReader br = null;
         StringBuilder sb = new StringBuilder();
@@ -29,6 +37,47 @@ public class FileHelper {
             }
         }
         return sb.toString();
+    }
+
+    public static List<String> readFileAsLines(File inItem) {
+        BufferedReader in = null;
+        List<String> lines = new ArrayList<>();
+        try {
+            in = new BufferedReader(
+                    new FileReader(inItem)
+            );
+            String s = in.readLine();
+            while (s != null) {
+                lines.add(s);
+                s = in.readLine();
+            }
+            in.close();
+            return lines;
+        } catch (IOException e) {
+            logger.error("IOException when read  data from file : {}", e);
+            return null;
+        }
+    }
+
+    public static List<String> readFileAsLines(String filePath) {
+        File file = new File(filePath);
+        return readFileAsLines(file);
+    }
+
+    public static List<String> processFile(String filePath, BufferdReaderProcessor p) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            return p.process(br);
+        }
+    }
+
+    public static List<String> processFile(File file, BufferdReaderProcessor p) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            return p.process(br);
+        }
+    }
+
+    public interface BufferdReaderProcessor {
+        List<String> process(BufferedReader b) throws IOException;
     }
 
 }
--- webporter-core/src/test/java/com/brianway/webporter/util/FileHelperTest.java
+++ webporter-core/src/test/java/com/brianway/webporter/util/FileHelperTest.java
@@ -1,19 +1,86 @@
 package com.brianway.webporter.util;
 
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by brian on 16/11/24.
  */
 public class FileHelperTest {
+    private static String baseDir;
+
+    @BeforeClass
+    public static void init() {
+        baseDir = FileHelperTest.class.getResource("/").getPath();
+    }
 
     @Test
-    public void testGetRawText(){
-        String path = this.getClass().getResource("/").getPath();
-        String file = path+"/"+ "config.json";
+    public void testGetRawText() {
+        String file = baseDir + "/" + "config.json";
         String rawText = FileHelper.getRawText(file);
         Assert.assertNotNull(rawText);
         Assert.assertTrue(rawText.contains("headers"));
+    }
+
+    @Test
+    public void testReadFileAsLines() {
+        String filePath = baseDir + "/line-file.html";
+        int lineNumber = 6;
+
+        List<String> lines = FileHelper.readFileAsLines(filePath);
+        Assert.assertNotNull(lines);
+        Assert.assertEquals(lineNumber, lines.size());
+
+        int nonEmptyNumber = 4;
+        int count = (int) lines.stream()
+                .filter(s -> !StringUtils.isEmpty(s))
+                .count();
+        Assert.assertEquals(nonEmptyNumber, count);
+    }
+
+    @Test
+    public void testProcessFileByPath() throws IOException {
+        String filePath = baseDir + "/line-file.html";
+        int nonEmptyNumber = 4;
+
+        List<String> content = FileHelper.processFile(filePath, (br) -> {
+            List<String> nonEmpties = new ArrayList<>();
+            String s;
+            while ((s = br.readLine()) != null) {
+                if (!StringUtils.isEmpty(s)) {
+                    nonEmpties.add(s);
+                }
+            }
+            return nonEmpties;
+        });
+        Assert.assertNotNull(content);
+        Assert.assertEquals(nonEmptyNumber, content.size());
+    }
+
+    @Test
+    public void testProcessFileByFile() throws IOException {
+        String filePath = baseDir + "/line-file.html";
+        File file = new File(filePath);
+        int emptyNumber = 2;
+
+        List<String> content = FileHelper.processFile(file, (br) -> {
+            List<String> empties = new ArrayList<>();
+            String s;
+            while ((s = br.readLine()) != null) {
+                if (StringUtils.isEmpty(s)) {
+                    empties.add(s);
+                }
+            }
+            return empties;
+        });
+        Assert.assertNotNull(content);
+        Assert.assertEquals(emptyNumber, content.size());
     }
 }
